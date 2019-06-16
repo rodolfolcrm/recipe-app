243f1a66d4a0d1f4deca01f031b7693e50e900c9
rodolfolcrm <rodolfolcrm@gmail.com>
2019-06-16 15:01:01 +0200

    Home page message improvements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,21 @@
 
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
-import { AppComponent } from "app/app.component";
 import { ShoppingListComponent } from "app/shopping-list/shopping-list.component";
 import { HomeComponent } from "app/core/home/home.component";
 import { AuthGuard } from "app/auth/auth-guard.service";
 
 
-const appRoutes: Routes =[
-  {path: '', component: HomeComponent},
-  {path: 'recipes', loadChildren: () => import('./recipies/recipes.module').then(m => m.RecipesModule), canLoad:[AuthGuard]},
-  {path: 'shopping-list', component: ShoppingListComponent}
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'recipes', loadChildren: () => import('./recipies/recipes.module').then(m => m.RecipesModule), canLoad: [AuthGuard] },
+  { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule]
 })
-export class AppRoutingModule{
+export class AppRoutingModule {
 
 }
--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { AuthService } from 'app/auth/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+@Injectable()
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit() {
   }
